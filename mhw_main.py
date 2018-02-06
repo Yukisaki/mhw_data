ce6ef775ea97c2b7f548cb0f0f3d5f305e260425
Yukisaki <83112307@qq.com>
2018-02-06 17:23:15 +0800

    Add files via upload

---
+++ mhw_main.py
@@ -0,0 +1,228 @@
+# -*- coding: UTF-8 -*-
+from urllib import request
+import urllib
+import requests
+import os
+import re
+import time
+from lxml import etree
+import json
+
+
+soubi = []
+soubi_list_1 = []
+soubi_list_2 = []
+soubi_list_3 = []
+soubi_list_4 = []
+soubi_list_5 = []
+
+def get_detail_url():
+    try:
+        hp_url = r'http://mh-world.net/series-armor-mhw.html'
+        testurl = r'http://mh-world.net/armor-mhw/series-カガチ.html'
+        testurl = r'http://mh-world.net/armor-mhw/series-ディアネロβ.html'
+        res = requests.get(hp_url)
+        root = etree.HTML(res.text)
+        reqlist = root.xpath('//tbody/tr/td/a')
+        for req_item in reqlist:
+            req_url = req_item.xpath('@href')[0]
+            res_sub = requests.get(req_url)
+            if res_sub.status_code == 404:
+                print('url: %s failed!' % req_url)
+                continue
+            root = etree.HTML(res_sub.text)
+            table_list = root.xpath("//table[@class='mum']")
+            rare_table = root.xpath("//table[@class='txt-c mum']")
+            for table in rare_table[0]:
+                if len(table) == 8:
+                    rare = int(table.xpath('td')[0].text)
+            for table in table_list:
+                if len(table.xpath('tbody/tr')[0]) == 5:
+                    for tr in table.xpath('tbody/tr'):
+                        soubi_dict = {'name': None, 'part': None, 'zokusei1': None, 'zokusei2': None, 'lv1': None, 'lv2': None, 'slot': None, 'rare': rare}
+                        for td in tr.xpath('td'):
+                            index = tr.xpath('td').index(td)
+                            if td.text:
+                                soubi_dict = get_soubi_dict(index, td.text, soubi_dict, 0)
+                                # print(td.text)
+                            else:
+                                for li in td.xpath('ul/li'):
+                                    zoukusei_index = td.xpath('ul/li').index(li)
+                                    soubi_dict = get_soubi_dict(index, li.xpath('text()')[0].lstrip('+'), soubi_dict, zoukusei_index, li.xpath('a')[0].text)
+                                    # print(li.xpath('a')[0].text, li.xpath('text()')[0])
+                        soubi_dict = complete_soubi_dict(soubi_dict)
+                        if soubi_dict['part'] == '頭':
+                            soubi_list_1.append(soubi_dict)
+                        elif soubi_dict['part'] == '胴':
+                            soubi_list_2.append(soubi_dict)
+                        elif soubi_dict['part'] == '腕':
+                            soubi_list_3.append(soubi_dict)
+                        elif soubi_dict['part'] == '腰':
+                            soubi_list_4.append(soubi_dict)
+                        elif soubi_dict['part'] == '脚':
+                            soubi_list_5.append(soubi_dict)
+            print('url: %s finished!' % req_url)
+            time.sleep(0.5)
+        f2write = open(r'data.txt', 'a', encoding='utf-8')
+        f2write.write(json.dumps({'list1': soubi_list_1}))
+        f2write.write('\n')
+        f2write.write(json.dumps({'list2': soubi_list_2}))
+        f2write.write('\n')
+        f2write.write(json.dumps({'list3': soubi_list_3}))
+        f2write.write('\n')
+        f2write.write(json.dumps({'list4': soubi_list_4}))
+        f2write.write('\n')
+        f2write.write(json.dumps({'list5': soubi_list_5}))
+        f2write.write('\n')
+        f2write.close()
+        print('all finished!')
+    except Exception as e:
+        print(e)
+
+def get_soubi_dict(index, value, soubi_dict, zokusei_index=0, zoukusei_name=None):
+    if index == 0:
+        soubi_dict['part'] = value
+    elif index == 1:
+        soubi_dict['name'] = value
+    elif index == 2:
+        soubi_dict['def'] = value
+    elif index == 3:
+        soubi_dict['slot'] = value
+    elif index == 4:
+        soubi_dict['zokusei' + str(zokusei_index + 1)] = zoukusei_name
+        soubi_dict['lv' + str(zokusei_index + 1)] = int(value)
+    return soubi_dict
+
+
+def complete_soubi_dict(soubi_dict):
+    number, (lv1, lv2, lv3) = get_slot_number(soubi_dict['slot'])
+    soubi_dict['slot_number'] = number
+    soubi_dict['slot_lv'] = {'lv1': lv1, 'lv2': lv2, 'lv3': lv3}
+    if soubi_dict['lv2']:
+        soubi_dict['total_skill'] = soubi_dict['lv1'] + soubi_dict['lv2']
+    else:
+        soubi_dict['total_skill'] = soubi_dict['lv1']
+    return soubi_dict
+
+
+def get_slot_number(slot):
+    slot_list = slot.split('/')
+    number = 3
+    lv1 = lv2 = lv3 = 0
+    for item in slot_list:
+        if item == '-':
+            number = number - 1
+        elif item == '1':
+            lv1 = lv1 + 1
+        elif item == '2':
+            lv2 = lv2 + 1
+        elif item == '3':
+            lv3 = lv3 + 1
+    return number, (lv1, lv2, lv3)
+
+
+def get_lv(name, part_dict):
+    if part_dict['zokusei1'] and part_dict['zokusei1'] == name:
+        return part_dict['lv1']
+    elif part_dict['zokusei2'] and part_dict['zokusei2'] == name:
+        return part_dict['lv2']
+    else:
+        return 0
+
+def zokusei_filter(zokusei, soubi_list):
+    soubi_list = list(filter(lambda x: x['zokusei1'] == zokusei or x['zokusei2'] == zokusei, soubi_list))
+    return soubi_list
+
+
+def get_filter_soubi_list(filter_keys, soubi_list):
+    filter_soubi_list = []
+    for key in filter_keys:
+        filter_soubi_list = filter_soubi_list + zokusei_filter(key, soubi_list)
+    return filter_soubi_list
+
+
+def get_filter_value(filter_condition_keys, part, filter_values_old):
+    filter_values = {}
+    for key in filter_condition_keys.keys():
+        filter_values[key] = get_lv(key, part)
+    if filter_values_old:
+        for key in filter_condition_keys.keys():
+            filter_values[key] = filter_values_old[key] + filter_values[key]
+    return filter_values
+
+
+def if_satisfied(filter_condition_keys, filter_values):
+    flag = True
+    for key in filter_condition_keys.keys():
+        flag = flag and (filter_values[key] >= filter_condition_keys[key])
+    return flag
+
+
+if __name__ == '__main__':
+    # get_detail_url()
+    f2write = open(r'data.txt', 'r', encoding='utf-8')
+    soubi = []
+    soubi_list_1 = []
+    soubi_list_2 = []
+    soubi_list_3 = []
+    soubi_list_4 = []
+    soubi_list_5 = []
+    rare = 5
+    for index, line in enumerate(f2write):
+        if index == 0:
+            soubi_list_1 = list(filter(lambda x: x['rare'] >= rare, json.loads(line)['list1']))
+        elif index == 1:
+            soubi_list_2 = list(filter(lambda x: x['rare'] >= rare, json.loads(line)['list2']))
+        elif index == 2:
+            soubi_list_3 = list(filter(lambda x: x['rare'] >= rare, json.loads(line)['list3']))
+        elif index == 3:
+            soubi_list_4 = list(filter(lambda x: x['rare'] >= rare, json.loads(line)['list4']))
+        elif index == 4:
+            soubi_list_5 = list(filter(lambda x: x['rare'] >= rare, json.loads(line)['list5']))
+    f2write.close()
+
+    filter_condition_keys = {u'弱点特効': 3, u'超会心': 0, u'体術': 3, u'回避性能': 1}
+    filter_soubi_list_1 = get_filter_soubi_list(filter_condition_keys.keys(), soubi_list_1)
+    filter_soubi_list_2 = get_filter_soubi_list(filter_condition_keys.keys(), soubi_list_2)
+    filter_soubi_list_3 = get_filter_soubi_list(filter_condition_keys.keys(), soubi_list_3)
+    filter_soubi_list_4 = get_filter_soubi_list(filter_condition_keys.keys(), soubi_list_4)
+    filter_soubi_list_5 = get_filter_soubi_list(filter_condition_keys.keys(), soubi_list_5)
+    for item1 in filter_soubi_list_1:
+        for item2 in filter_soubi_list_2:
+            for item3 in filter_soubi_list_3:
+                for item4 in filter_soubi_list_4:
+                    for item5 in filter_soubi_list_5:
+                        soubi.append([item1, item2, item3, item4, item5])
+
+    for comb in soubi:
+        tokkou = 0
+        taijutu = 0
+        kaisin = 0
+        sutamina = 0
+        kaihi = 0
+        total_skill_level = 0
+        total_lv1_slot = total_lv2_slot = total_lv3_slot = 0
+        slot_num = 0
+        filter_values = None
+        for part in comb:
+            total_skill_level = total_skill_level + part['total_skill']
+            total_lv1_slot = total_lv1_slot + part['slot_lv']['lv1']
+            total_lv2_slot = total_lv2_slot + part['slot_lv']['lv2']
+            total_lv3_slot = total_lv3_slot + part['slot_lv']['lv3']
+            slot_num = slot_num + part['slot_number']
+            # tokkou = tokkou + get_lv(u'弱点特効', part)
+            # taijutu = taijutu + get_lv(u'体術', part)
+            # kaisin = kaisin + get_lv(u'超会心', part)
+            # sutamina = sutamina + get_lv(u'スタミナ急速回復', part)
+            # kaihi = kaihi + get_lv(u'回避性能', part)
+            filter_values = get_filter_value(filter_condition_keys, part, filter_values)
+        # if kaihi >= 5:
+        # if tokkou == 3 and kaisin == 1 and taijutu >= 3 and kaihi >= 0:
+        if if_satisfied(filter_condition_keys, filter_values):
+        # if total_skill_level >= 15:
+            soubi_st = ''
+            for x in comb:
+                soubi_st = soubi_st + x['name'] + ' ' + x['zokusei1'] + ' lv(' + str(x['lv1']) + ')' + (', ' + x['zokusei2'] + ' lv(' + str(x['lv2']) + ') [' + x['slot'] +  '] / ' if x['zokusei2'] else ' [' + x['slot'] + '] / ')
+            print(soubi_st)
+            print('slot: %s, total_skill_lv: %s, lv1_slot: %s, lv2_slot: %s, lv3_slot: %s' % (slot_num, total_skill_level, total_lv1_slot, total_lv2_slot, total_lv3_slot))
+            print(filter_values)
